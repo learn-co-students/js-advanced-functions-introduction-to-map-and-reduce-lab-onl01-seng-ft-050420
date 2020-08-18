e2a84d7c7677878e1276728ab5a889246285418f
gd-m <gitanjalidmule@gmail.com>
2020-08-18 18:24:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map((num) => num * -1)
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map(num => num)
+}
+
+function mapToDouble(array){
+    return array.map(num => num * 2)
+}
+
+function mapToSquare(array){
+    return array.map(num => num * num)
+}
+
+
+function reduceToTotal(array, startingPoint=0) {
+    return array.reduce((acc, val) => {
+        return acc + val
+    }, startingPoint)
+}
+
+function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+  
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
