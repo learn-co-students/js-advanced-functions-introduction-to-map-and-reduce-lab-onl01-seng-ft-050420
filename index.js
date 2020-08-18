53e05ced1449454c48033a8d791b35a98630b9fc
leobr <leobrionesherrera@gmail.com>
2020-08-17 21:16:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function mapToNegativize(array) {
+    return array.map(x => -x);
+}
+
+function mapToNoChange(array) {
+    return array;
+}
+
+function mapToDouble(array) {
+    return array.map(x => x * 2);
+}
+
+function  mapToSquare(array) {
+    return array.map(x => x**2);
+}
+
+function reduceToTotal(array, starting=0) {
+    return array.reduce((total, cur ) => {return cur + total}, starting)
+}
+
+function reduceToAllTrue(array) {
+    for (const el of array) {
+        if (!el){
+            return false;
+        }
+    }
+    return true;
+}
+
+function reduceToAnyTrue(array) {
+    for (const el of array) {
+        if (!!el){
+            return true;
+        }
+    }
+    return false;
+}
